52695d2198907e1b5ac74d4628b1044432e41d42
Kevin Ramos <kevramos1121@gmail.com>
2021-05-07 20:50:14 -0400

    Fixed scss file to automate css file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 import '../css/style.scss';
+
+const header = "Webpack SAAS";
+
+const slideshows = document.querySelectorAll('[data-component="slideshow"]');
+function initSlideShow(slideshow) {
+    const slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+    let index = 0;
+    const time = 1500;
+    slides[index].classList.add('active');
+    setInterval(() => {
+        slides[index].classList.remove('active');
+        index += 1;
+        if (index === slides.length) index = 0;
+        slides[index].classList.add('active');
+        console.log(slides);
+    }, time);
+}
+slideshows.forEach(initSlideShow);
